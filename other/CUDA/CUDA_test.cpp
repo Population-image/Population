71480c408a6af25af6f4c286772596930368963c
Pierre Louis Aublin <plaublin@alphanumericvision.fr>
2015-02-04 14:35:50 +0100

    cuda++

--- other/CUDA/CUDA_test.cpp
+++ other/CUDA/CUDA_test.cpp
@@ -3,21 +3,25 @@
 #include "popconfig.h"
 #include "popcuda.h"
 #include "cuda_test.h"
+#include "c_neural_net.h"
 
 int main(){
-#if defined(HAVE_CUDA)
-    std::cout << "You have CUDA support.";
+	test_neural_net();
+	return 0;
 
-    if (popcuda::isCudaAvailable()) {
-        std::cout << " And you have a CUDA device." << std::endl << std::endl;
-        cuda_test();
-    } else {
-        std::cout << " But you don't have a CUDA device." << std::endl;
-    }
+#if defined(HAVE_CUDA)
+	std::cout << "You have CUDA support.";
+
+	if (popcuda::isCudaAvailable()) {
+		std::cout << " And you have a CUDA device." << std::endl << std::endl;
+		cuda_test();
+	} else {
+		std::cout << " But you don't have a CUDA device." << std::endl;
+	}
 
 #else
-    std::cout << "You do not have CUDA support :(" << std::endl;
+	std::cout << "You do not have CUDA support :(" << std::endl;
 #endif
 
-    return 0;
+	return 0;
 }
