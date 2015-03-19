7c2906d1987d21d6fa5e064aed0e7c9e04e6bd34
Pierre Louis Aublin <plaublin@alphanumericvision.fr>
2015-03-19 13:38:30 +0100

    refactoring for cuda

--- other/CUDA/popcuda.h
+++ other/CUDA/popcuda.h
@@ -8,6 +8,7 @@
 #if defined(HAVE_CUDA)
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <cublas_v2.h>
 
 /*
  * gpuErrorCheck is a macro to wrap every CUDA call and verify that they succeded. See http://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
@@ -34,6 +35,13 @@ namespace popcuda {
 /* return true if cuda is enabled and there is a cuda device */
 bool isCudaAvailable();
 
+#if defined(HAVE_CUDA)
+const char* cublasGetErrorString(cublasStatus_t status);
+
+unsigned int getMaxNumberThreadsPerBlock();
+#endif
+
 }
+
 
 #endif
