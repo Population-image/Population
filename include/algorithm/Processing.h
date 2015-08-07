9f66fb440d9cd0d306cee97b303529babf5b5c57
VT <vtariel@alphanumericvision.fr>
2015-08-07 17:40:25 +0200

    Random geometry article

--- include/algorithm/Processing.h
+++ include/algorithm/Processing.h
@@ -388,70 +388,7 @@ struct POP_EXPORTS Processing
         }
         return label;
     }
-//    \cond HIDDEN_SYMBOLS
-    struct __FunctorNiblackMethod
-    {
-        const Mat2UI32* _integral;
-        const Mat2UI32* _integral_power_2;
-        F32 area_minus1;
-        F32 _k;
-        int _radius;
-        F32 _offset_value;
-        __FunctorNiblackMethod(const Mat2UI32 & integral,const Mat2UI32& integral_power_2,F32 _area_minus1,F32 k,int radius,F32 offset_value)
-            :_integral(&integral),_integral_power_2(&integral_power_2),area_minus1(_area_minus1),_k(k),_radius(radius),_offset_value(offset_value){
 
-        }
-
-        template<typename PixelType>
-        UI8 operator()(const MatN<2,PixelType > & f,const  typename MatN<2,PixelType>::E & x){
-            Vec2I32 xadd1=x+Vec2I32(_radius);
-            Vec2I32 xadd2=x+Vec2I32(-_radius);
-            Vec2I32 xsub1=x-Vec2I32(_radius,-_radius);
-            Vec2I32 xsub2=x-Vec2I32(-_radius,_radius);
-            F32 mean =(F32) (*_integral)(xadd1)+(*_integral)(xadd2)-(*_integral)(xsub1)-(*_integral)(xsub2);
-            mean*=area_minus1;
-
-            F32 standartdeviation = static_cast<F32>((*_integral_power_2)(xadd1)+(*_integral_power_2)(xadd2)-(*_integral_power_2)(xsub1)-(*_integral_power_2)(xsub2));
-            standartdeviation*=area_minus1;
-            standartdeviation =standartdeviation-mean*mean;
-
-            if(standartdeviation>0)
-                standartdeviation = std::sqrt( standartdeviation);
-            else
-                return  0;
-            if(f(x-_radius)>ArithmeticsSaturation<PixelType,F32>::Range( mean+_k*standartdeviation)-_offset_value)
-                return 255;
-            else
-                return  0;
-        }
-    };
-    struct __FunctorMean
-    {
-        const Mat2UI32* _integral;
-        F32 area_minus1;
-        int _radius;
-        F32 _offset_value;
-        __FunctorMean(const Mat2UI32 & integral,F32 _area_minus1,int radius,F32 offset_value)
-            :_integral(&integral),area_minus1(_area_minus1),_radius(radius),_offset_value(offset_value){
-
-        }
-
-        template<typename PixelType>
-        UI8 operator()(const MatN<2,PixelType > & f,const  typename MatN<2,PixelType>::E & x){
-            Vec2I32 xadd1=x+Vec2I32(_radius);
-            Vec2I32 xadd2=x+Vec2I32(-_radius);
-            Vec2I32 xsub1=x-Vec2I32(_radius,-_radius);
-            Vec2I32 xsub2=x-Vec2I32(-_radius,_radius);
-            F32 mean =(F32) (*_integral)(xadd1)+(*_integral)(xadd2)-(*_integral)(xsub1)-(*_integral)(xsub2);
-            mean*=area_minus1;
-
-            if(f(x-_radius)>ArithmeticsSaturation<PixelType,F32>::Range( mean)-_offset_value)
-                return 255;
-            else
-                return  0;
-        }
-    };
-//\endcond
     /*!
      * \brief Niblack threshold (1986), An introduction to Digital Image Processing, Prentice-Hall
      * \param f input function
@@ -2752,6 +2689,70 @@ without  the application of greylevelRemoveEmptyValue, all grey-level excepted 0
         return thresholdOtsuMethod(f,thresholdvalue);
     }
 #endif
+    //    \cond HIDDEN_SYMBOLS
+        struct __FunctorNiblackMethod
+        {
+            const Mat2UI32* _integral;
+            const Mat2UI32* _integral_power_2;
+            F32 area_minus1;
+            F32 _k;
+            int _radius;
+            F32 _offset_value;
+            __FunctorNiblackMethod(const Mat2UI32 & integral,const Mat2UI32& integral_power_2,F32 _area_minus1,F32 k,int radius,F32 offset_value)
+                :_integral(&integral),_integral_power_2(&integral_power_2),area_minus1(_area_minus1),_k(k),_radius(radius),_offset_value(offset_value){
+
+            }
+
+            template<typename PixelType>
+            UI8 operator()(const MatN<2,PixelType > & f,const  typename MatN<2,PixelType>::E & x){
+                Vec2I32 xadd1=x+Vec2I32(_radius);
+                Vec2I32 xadd2=x+Vec2I32(-_radius);
+                Vec2I32 xsub1=x-Vec2I32(_radius,-_radius);
+                Vec2I32 xsub2=x-Vec2I32(-_radius,_radius);
+                F32 mean =(F32) (*_integral)(xadd1)+(*_integral)(xadd2)-(*_integral)(xsub1)-(*_integral)(xsub2);
+                mean*=area_minus1;
+
+                F32 standartdeviation = static_cast<F32>((*_integral_power_2)(xadd1)+(*_integral_power_2)(xadd2)-(*_integral_power_2)(xsub1)-(*_integral_power_2)(xsub2));
+                standartdeviation*=area_minus1;
+                standartdeviation =standartdeviation-mean*mean;
+
+                if(standartdeviation>0)
+                    standartdeviation = std::sqrt( standartdeviation);
+                else
+                    return  0;
+                if(f(x-_radius)>ArithmeticsSaturation<PixelType,F32>::Range( mean+_k*standartdeviation)-_offset_value)
+                    return 255;
+                else
+                    return  0;
+            }
+        };
+        struct __FunctorMean
+        {
+            const Mat2UI32* _integral;
+            F32 area_minus1;
+            int _radius;
+            F32 _offset_value;
+            __FunctorMean(const Mat2UI32 & integral,F32 _area_minus1,int radius,F32 offset_value)
+                :_integral(&integral),area_minus1(_area_minus1),_radius(radius),_offset_value(offset_value){
+
+            }
+
+            template<typename PixelType>
+            UI8 operator()(const MatN<2,PixelType > & f,const  typename MatN<2,PixelType>::E & x){
+                Vec2I32 xadd1=x+Vec2I32(_radius);
+                Vec2I32 xadd2=x+Vec2I32(-_radius);
+                Vec2I32 xsub1=x-Vec2I32(_radius,-_radius);
+                Vec2I32 xsub2=x-Vec2I32(-_radius,_radius);
+                F32 mean =(F32) (*_integral)(xadd1)+(*_integral)(xadd2)-(*_integral)(xsub1)-(*_integral)(xsub2);
+                mean*=area_minus1;
+
+                if(f(x-_radius)>ArithmeticsSaturation<PixelType,F32>::Range( mean)-_offset_value)
+                    return 255;
+                else
+                    return  0;
+            }
+        };
+    //\endcond
 };
 /** @}*/
 template<typename Function>
