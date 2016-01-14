95ec80bafa3587a5916bdd17797b4070e4d6bbc2
dokhanh <qkdo@alphanumericvision.fr>
2016-01-14 11:27:23 +0100

    remove unnecessary things in the rectangle detection

--- include/data/notstable/CharacteristicCluster.h
+++ include/data/notstable/CharacteristicCluster.h
@@ -66,91 +66,6 @@ public:
 
 };
 
-template<typename Position=Vec2I32>
-class CharacteristicBoundingBoxForRectangle : public CharacteristicBoundingBox<Position> {
-    std::vector<Position> _set_points;
-    // besides _xmin and _xmax, we also keep track of the other two corners of the (supposed) rectangle
-    Position _cornerUpRight, _cornerLowLeft;
-public:
-    inline std::vector<Position> getVectorPoints() const {
-        return _set_points;
-    }
-
-    inline Position getCornerUpRight() const {
-        return _cornerUpRight;
-    }
-
-    inline Position getCornerLowLeft() const {
-        return _cornerLowLeft;
-    }
-
-    virtual void addPoint(const Position & x) {
-        CharacteristicBoundingBox<Position>::addPoint(x);
-        _cornerUpRight(0) = CharacteristicBoundingBox<Position>::getMin()(0);
-        _cornerUpRight(1) = CharacteristicBoundingBox<Position>::getMax()(1);
-        _cornerLowLeft(0) = CharacteristicBoundingBox<Position>::getMax()(0);
-        _cornerLowLeft(1) = CharacteristicBoundingBox<Position>::getMin()(1);
-        _set_points.push_back(x);
-    }
-
-    // to see if a point x is in the upper-left, upper-right, lower-left, or lower-right region of the (supposed) rectangle
-    // output:
-    // 0 : upper left
-    // 1 : upper right
-    // 2 : lower left
-    // 3 : lower right
-    int getCorner(const Position& x) {
-        int res;
-        if (x(0) < CharacteristicBoundingBox<Position>::getCenter()(0)) {
-            return (x(1) < CharacteristicBoundingBox<Position>::getCenter()(1)? 0 : 1);
-        } else {
-            return (x(1) < CharacteristicBoundingBox<Position>::getCenter()(1)? 2 : 3);
-        }
-        return res;
-    }
-
-    F32 distanceToBound() {
-        F32 sumDistance = 0;
-        Position nearestCorner;
-        for (typename std::vector<Position>::iterator it = getVectorPoints().begin() ; it != getVectorPoints().end() ; ++ it) {
-            switch (getCorner(*it)) {
-                case 0 : {
-                    nearestCorner = CharacteristicBoundingBox<Position>::getMin();
-                    break;
-                }
-                case 1 : {
-                    nearestCorner = _cornerUpRight;
-                    break;
-                }
-                case 2 : {
-                    nearestCorner = _cornerLowLeft;
-                    break;
-                }
-                case 3 : {
-                    nearestCorner = CharacteristicBoundingBox<Position>::getMax();
-                    break;
-                }
-                default : {
-                    std::cerr << "Value of getCorner() is not valid" << std::endl;
-                    break;
-                }
-            }
-            Position distance(nearestCorner - *it);
-            sumDistance += std::min(std::abs(distance(0)), std::abs(distance(1)));
-            //sumDistance += std::abs(distance(0));
-        }
-        return sumDistance;
-    }
-
-    F32 getHeight() {
-        return CharacteristicBoundingBox<Position>::getSize()(0);
-    }
-
-    F32 getWidth() {
-        return CharacteristicBoundingBox<Position>::getSize()(1);
-    }
-};
-
 template<int DIM,typename TypePixel>
 class CharacteristicGreyLevel
 {
@@ -268,52 +183,6 @@ struct CharacteristicClusterMix :  CharacteristicMass, CharacteristicBoundingBox
     }
 };
 
-template<int DIM, typename TypePixel>
-struct CharacteristicClusterMixForRectangle : CharacteristicMass, CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >, CharacteristicGreyLevel<DIM, TypePixel > {
-    void addPoint(const VecN<DIM, I32> & x) {
-        CharacteristicMass::addPoint(x);
-        CharacteristicBoundingBoxForRectangle<VecN<DIM, I32 > >::addPoint(x);
-        CharacteristicGreyLevel<DIM, TypePixel>::addPoint(x);
-    }
-
-    bool isEmpty() {
-        return (CharacteristicMass::getMass() == 0);
-    }
-
-    F32 averageDistanceToBound() {
-        return CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::distanceToBound()/CharacteristicMass::getMass();
-    }
-
-    F32 averageDistanceToBoundOverSize() {
-        F32 distance = averageDistanceToBound();
-        F32 size = std::min(CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getWidth(), CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getHeight());
-        if (size > 0 ) {
-            return distance / size;
-        } else {
-            // an arbitrarily large value
-            return 1000.f;
-        }
-    }
-
-    bool isRectangle() {
-        F32 size = std::min(CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getWidth(), CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getHeight());
-        if (!isEmpty() && size > 50) {
-            std::cout << averageDistanceToBoundOverSize() << std::endl;
-        }
-        return (!isEmpty() && size > 50 && averageDistanceToBoundOverSize() < 0.2 && (CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getWidth() > CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getHeight()));
-    }
-
-    // return a vector of 4 corners of the rectangle
-    std::vector<pop::Vec2I32> getVertex() {
-        std::vector<pop::Vec2I32> vertex;
-        vertex.push_back(CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getMin());
-        vertex.push_back(CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getCornerUpRight());
-        vertex.push_back(CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getCornerLowLeft());
-        vertex.push_back(CharacteristicBoundingBoxForRectangle<VecN<DIM, I32> >::getMax());
-        return vertex;
-    }
-};
-
 template<int DIM,typename TypePixel>
 struct CharacteristicClusterMixListPoints :  CharacteristicMass, CharacteristicBoundingBox<VecN<DIM,I32> >, CharacteristicGreyLevel<DIM ,TypePixel >, ListOfPoints<VecN<DIM, I32 > >
 {
@@ -330,14 +199,6 @@ struct CharacteristicClusterMixListPoints :  CharacteristicMass, CharacteristicB
             mat(*it - CharacteristicBoundingBox<VecN<DIM, I32 > >::getMin()) = 255;
         }
         return mat;
-    }
-
-    VecN<DIM, I32 > getMax() const {
-        return CharacteristicBoundingBox<VecN<DIM, I32 > >::getMax();
-    }
-
-    VecN<DIM, I32 > getMin() const {
-        return CharacteristicBoundingBox<VecN<DIM, I32 > >::getMin();
     }
 };
 
